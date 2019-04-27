fbb70adecb2084eef211d541afe3b1734e3ea922
bruasu <bruasu@gmail.com>
2019-04-27 16:37:35 -0300

    add service the game, connection with websocket.io

---
+++ service-game/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//start the server
+const server = app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+});
+
+//websockets
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', (socket) =>{
+
+    console.log('new connection', socket.id);
+
+    socket.on('test:message', (data) =>{
+        console.log(data);  
+        socket.emit('test2', data);          
+    });
+});
