2a44ff8694d92d2aaf30dd9e703f51fd4707a1e1
lucasasu <lucasasu008@gmail.com>
2019-04-27 16:28:11 -0300

    act 2

--- MultiplayerSnake/src/com/asubrothers/multiplayersnacke/game/Game.java
+++ MultiplayerSnake/src/com/asubrothers/multiplayersnacke/game/Game.java
@@ -1,13 +1,15 @@
 package com.asubrothers.multiplayersnacke.game;
 
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JPanel;
 
-public class Game extends JPanel{
+public class Game extends JPanel implements KeyListener{
 
 	public Game() {
-		JButton button = new JButton("estas jugando");
-		this.add(button);
+		
 	}
 	public void update() {
 		
@@ -15,4 +17,20 @@ public class Game extends JPanel{
 	public void render() {
 		
 	}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
